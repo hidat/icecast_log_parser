708c50a27afc803b56f5cf4a82293c8be203b252
Craig Huizenga <hidatch@gmail.com>
2018-06-07 10:33:44 -0700

    Adding ability to read parameters from the console.

--- sx_log_generator/sx_log_generator/Program.cs
+++ sx_log_generator/sx_log_generator/Program.cs
@@ -10,9 +10,8 @@ namespace LogGenerator
     {
         static void Main(string[] args)
         {
-            string streamUrl = "stream";
 
-            if (args.Length < 3)
+            if (args.Length == 1 && (args[0] == "-h" || args[0].ToLower() == "help"))
             {
                 Console.WriteLine("Usage:  log_generator source destination stream-id [source-url]");
                 Console.WriteLine("  source: Directory or file to process. Directories should end with a '\\', or the source will be treated as a file.");
@@ -22,19 +21,93 @@ namespace LogGenerator
                 return;
             }
 
+            string sourceDirectory = null;
+            string destinationFile = null;
+            string channelId = null;
+            string streamUrl = null;
+
+            if (args.Length > 0)
+            {
+                sourceDirectory = args[0];
+            }
+
+            if (args.Length > 1)
+            {
+                destinationFile = args[1];
+            }
+
+            if (args.Length > 2)
+            {
+                channelId = args[2];
+            }
+
             if (args.Length > 3)
             {
                 streamUrl = args[3];
             }
 
-            var parser = new FileParser(args[0], args[1], args[2], streamUrl);
+            Console.WriteLine("Icecast Log Parser\n");
+            if (String.IsNullOrEmpty(sourceDirectory))
+            {
+                string val = null;
+                Console.Write("Source Directory: ");
+                val = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(val))
+                {
+                    return;
+                }
+                sourceDirectory = val;
+            }
+
+            if (String.IsNullOrEmpty(destinationFile))
+            {
+                string val = null;
+                Console.Write("Destination File: ");
+                val = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(val))
+                {
+                    return;
+                }
+                destinationFile = val;
+            }
+
+            if (String.IsNullOrEmpty(channelId))
+            {
+                string val = null;
+                Console.Write("Channel ID: ");
+                val = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(val))
+                {
+                    return;
+                }
+                channelId = val;
+            }
+
+            if (String.IsNullOrEmpty(streamUrl))
+            {
+                string val = null;
+                Console.Write("Stream Url: ");
+                val = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(val))
+                {
+                    return;
+                }
+                streamUrl = val;
+            }
+
+            Console.WriteLine("\nGenerating logs for:");
+            Console.WriteLine("  Source Directory: {0}", sourceDirectory);
+            Console.WriteLine("  Destination File: {0}", destinationFile);
+            Console.WriteLine("  Channel ID: {0}", channelId);
+            Console.WriteLine("  Stream URL: {0}", streamUrl);
+
+            var parser = new FileParser(sourceDirectory, destinationFile, channelId, streamUrl);
             if (!parser.isValid)
             {
-                Console.WriteLine("Unable to find or access {0]", args[0]);
+                Console.WriteLine("Unable to find or access {0]", sourceDirectory);
             }
             else
             {
-                Console.WriteLine("Processing {0}.", args[0]);
                 var lines = parser.parse();
                 Console.WriteLine("{0} lines scanned in {1} files, {2} lines found.", parser.linesProcessed, parser.filesProcessed, lines);
             }
